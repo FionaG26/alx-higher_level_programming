a78f099f2b3198dff1c03150e11c3ae007842242
FionaG26 <fionamuthoni18@gmail.com>
2022-11-29 14:10:09 +0000

    Task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(100):
-        if i == 99:
-                print(i)
-        else:
-                print('{:02}'.format(i), end=', ')
+for number in range(0, 100):
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end="
