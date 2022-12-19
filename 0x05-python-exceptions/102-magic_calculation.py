0cb0e5e06aa48621393325b128b72d54561e2f27
FionaG26 <fionamuthoni18@gmail.com>
2022-12-19 10:59:04 +0000

    Tasks

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -5,8 +5,9 @@ def magic_calculation(a, b):
         try:
             if i > a:
                 raise Exception('Too far')
-            result += (a)**(b) / i
-        except:
+            else:
+                result += (a ** b) / i
+        except Exception:
             result = b + a
             break
     return (result)
