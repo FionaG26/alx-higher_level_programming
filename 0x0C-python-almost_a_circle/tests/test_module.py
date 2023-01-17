03971a1b0af4c7e42850f89960712e3c037ea3f4
FionaG26 <fionamuthoni18@gmail.com>
2023-01-16 23:05:45 -0800

    Task 0

--- 0x0C-python-almost_a_circle/tests/test_module.py
+++ 0x0C-python-almost_a_circle/tests/test_module.py
@@ -1,23 +1,13 @@
 import unittest
 
-class LearningCase(unittest.TestCase):
-    def test_starting_out(self):
-        self.assertEqual(1, 1)
 
-def main():
+class TestSum(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+
+    def test_sum_tuple(self):
+        self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
+
+if __name__ == '__main__':
     unittest.main()
-
-if __name__ == "__main__":
-    main()
-
-
-def test_starting_out():
-    assert 1 == 1
-
-
-cd /path/to/dir/
-python test_unittesting.py
-
-
-cd /path/to/dir/
-py.test
