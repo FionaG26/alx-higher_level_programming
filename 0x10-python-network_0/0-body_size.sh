b036dd893ec6a7a8606321ecf64aafc82743a0c7
FionaG26 <fionamuthoni18@gmail.com>
2023-03-30 06:10:53 -0700

    tasks

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -s $1 | wc -c | xargs printf "Size of the body: %d bytes\n"
-
+# This script takes a URL, sends a request to that URL, and displays the size of the body of the response in bytes
+curl -s $1 | wc -c
