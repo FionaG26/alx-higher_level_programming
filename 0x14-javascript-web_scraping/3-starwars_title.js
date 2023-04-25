8b0fd07fdad694f167a256616d8c63fc9ad13839
FionaG26 <fionamuthoni18@gmail.com>
2023-04-25 10:27:06 +0000

    tasks

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,9 +4,9 @@ const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
 request(url, function (error, response, body) {
-if (error) {
-console.error(error);
-} else {
-console.log(JSON.parse(body).title);
-}
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
 });
