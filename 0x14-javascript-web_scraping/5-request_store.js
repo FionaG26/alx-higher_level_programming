586ce4cc62b5dfbf1cfe2bd632f316544863aad2
FionaG26 <fionamuthoni18@gmail.com>
2023-04-25 11:27:22 +0000

    tasks

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, function (error, response, body) {
+if (error) {
+console.error(error);
+} else {
+fs.writeFile(filePath, body, 'utf-8', function (error) {
+if (error) {
+console.error(error);
+}
+});
+}
+});
