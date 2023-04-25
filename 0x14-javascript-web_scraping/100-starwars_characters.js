9476ef36a27083c9575e5d572d22c0dd9d3c6bd3
FionaG26 <fionamuthoni18@gmail.com>
2023-04-25 13:56:19 +0000

    tasks

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const characters = JSON.parse(body).characters;
+
+  for (const characterUrl of characters) {
+    request(characterUrl, function (error, response, body) {
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      const character = JSON.parse(body);
+      console.log(character.name);
+    });
+  }
+});
