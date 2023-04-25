de77b07f15767a1cb5c1c74329e22d9c029b912b
FionaG26 <fionamuthoni18@gmail.com>
2023-04-25 12:28:03 +0000

    tasks

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,20 +1,22 @@
 #!/usr/bin/node
+
 const request = require('request');
+
 const apiUrl = process.argv[2];
-const characterId = 18; // ID of Wedge Antilles character
+const characterId = '18';
+let count = 0;
+
 request(apiUrl, function (error, response, body) {
   if (error) {
     console.error(error);
     return;
   }
   const films = JSON.parse(body).results;
-  const count = films.reduce((acc, film) => {
+  for (const film of films) {
     const characters = film.characters;
     if (characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-      return acc + 1;
-    } else {
-      return acc;
+      count++;
     }
-  }, 0);
+  }
   console.log(count);
 });
