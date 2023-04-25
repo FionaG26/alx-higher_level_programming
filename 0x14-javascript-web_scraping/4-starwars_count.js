cbe6d95e083b8a01159cecb248e09c86431398e1
FionaG26 <fionamuthoni18@gmail.com>
2023-04-25 13:50:55 +0000

    tasks

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,21 +3,17 @@ const request = require('request');
 
 const apiUrl = process.argv[2];
 const characterId = '18';
-
 request(apiUrl, function (error, response, body) {
   if (error) {
     console.error(error);
     return;
   }
-
   const films = JSON.parse(body).results;
   let count = 0;
-
   for (const film of films) {
     if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
       count++;
     }
   }
-
   console.log(count);
 });
