e6b75b3033a4f64978973beb7b0b6894c7f9572a
FionaG26 <fionamuthoni18@gmail.com>
2023-04-25 09:29:05 +0000

    tasks

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+
+fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
